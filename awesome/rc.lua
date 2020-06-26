95d612928a288457bfe463cc2e8f8d628a03d450
Filip Dutescu <filip.dutescu@gmail.com>
2020-06-26 22:16:35 +0300

    updated awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -207,7 +207,7 @@ awful.screen.connect_for_each_screen(function(s)
             s.mytaglist,
             s.mypromptbox,
         },
---       s.mytasklist, -- Middle widget
+       s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
