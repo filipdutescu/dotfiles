20a50e9c76e8093a63ef6365045a06f4ca9ed7e4
Filip Dutescu <filip.dutescu@gmail.com>
2020-06-28 12:43:43 +0300

    updated awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -317,7 +317,7 @@ globalkeys = gears.table.join(
 
     -- Browser
     awful.key({ modkey },            "b",     function () awful.util.spawn(browser) end,
-              {description = "open browser", group = "applications"}),
+              {description = "open browser", group = "programs"}),
 
     awful.key({ modkey }, "x",
               function ()
@@ -479,7 +479,8 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer"},
+          "xtightvncviewer",
+					"org.remmina.Remmina"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
