1acf43db8370d4c2a00c2af9360d3471cddc75cf
Filip Dutescu <filip.dutescu@gmail.com>
2020-07-01 22:49:18 +0300

    restructuring folders

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -500,7 +500,7 @@ awful.rules.rules = {
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    { rule = { class = "Firefox" },
+    { rule = { class = "firefox" },
     	properties = { screen = 1, tag = "2" } },
 }
 -- }}}
