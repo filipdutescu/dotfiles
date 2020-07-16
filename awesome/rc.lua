69263084f29b283ea87b751c136ccfb3a352ebc5
Filip Dutescu <filip.dutescu@gmail.com>
2020-07-16 22:35:11 +0300

    minor updates updates

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -45,7 +45,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/default.lua")
+--beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/default.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = os.getenv("TERMINAL") or "alacritty"
@@ -110,6 +111,10 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
+-- Calendar popup widget
+calendar = awful.widget.calendar_popup.month()
+calendar:attach(mytextclock, 'tr')
+
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
                     awful.button({ }, 1, function(t) t:view_only() end),
@@ -150,25 +155,7 @@ local tasklist_buttons = gears.table.join(
                                               awful.client.focus.byidx(-1)
                                           end))
 
---local function set_wallpaper(s)
---    -- Wallpaper
---    if beautiful.wallpaper then
---        local wallpaper = beautiful.wallpaper
---        -- If wallpaper is a function, call it with the screen
---        if type(wallpaper) == "function" then
---            wallpaper = wallpaper(s)
---        end
---        gears.wallpaper.maximized(wallpaper, s, true)
---    end
---end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
---screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
---    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[2])
 
@@ -480,7 +467,8 @@ awful.rules.rules = {
           "Wpa_gui",
           "veromix",
           "xtightvncviewer",
-		  "org.remmina.Remmina"},
+					"org.remmina.Remmina",
+					"Steam"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
@@ -574,3 +562,4 @@ beautiful.useless_gap = 7
 
 -- Autostart
 awful.spawn.with_shell("~/.fehbg")
+awful.spawn.with_shell("rivalcfg -s 500 -S 1000 -e breath")
---
+++ awesome/widgets/battery.lua
@@ -0,0 +1,3 @@
+local awful = require("awful")
+local gears = require("gears")
+
